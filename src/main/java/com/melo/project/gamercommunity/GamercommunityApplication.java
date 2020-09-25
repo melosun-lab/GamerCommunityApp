ccb2c0a5c31089bace6e27d036815d365665c621
Chuhua Sun <chs090@ucsd.edu>
2020-09-24 17:11:53 -0700

    first commit

---
+++ src/main/java/com/melo/project/gamercommunity/GamercommunityApplication.java
@@ -0,0 +1,13 @@
+package com.melo.project.gamercommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamercommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamercommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melo/project/gamercommunity/GamercommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melo.project.gamercommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamercommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
