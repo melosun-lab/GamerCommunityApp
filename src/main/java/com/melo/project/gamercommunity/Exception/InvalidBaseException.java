5195cd2c6fca4e1fe303831fe2daf57c2e051127
Chuhua Sun <chs090@ucsd.edu>
2020-09-29 18:30:03 -0700

    Edit Validation

---
+++ src/main/java/com/melo/project/gamercommunity/Exception/InvalidBaseException.java
@@ -0,0 +1,15 @@
+package com.melo.project.gamercommunity.Exception;
+
+import org.springframework.http.HttpStatus;
+
+public abstract class InvalidBaseException extends Exception {
+
+    private HttpStatus errorCode;
+    private String errorMessage;
+
+    public InvalidBaseException(HttpStatus errorCode, String errorMessage) {
+        super();
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+}
