5c9a00a339c70814193884f5c17b3d95de49dcae
Chuhua Sun <chs090@ucsd.edu>
2020-10-02 17:35:31 -0700

    Added Security

---
+++ src/main/java/com/melo/project/gamercommunity/Constant/SecurityConstant.java
@@ -0,0 +1,5 @@
+package com.melo.project.gamercommunity.Constant;
+
+public class SecurityConstant {
+    public static final String AUTH_LOGIN_URL = "/login";
+}
---
+++ src/main/java/com/melo/project/gamercommunity/Security/CustomizedAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.melo.project.gamercommunity.Security;
+
+import com.melo.project.gamercommunity.Constant.SecurityConstant;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomizedAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public CustomizedAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        setFilterProcessesUrl(SecurityConstant.AUTH_LOGIN_URL);
+    }
+
+//    @Override
+//    public Authentication attempAuthentication(HttpServletRequest request,
+//                                               HttpServletResponse response) throws AuthenticationException{
+//        var username = request.getParameter("username");
+//        var password = request.getParameter("password");
+//        var authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+//
+//        return authenticationManager.authenticate(authenticationToken);
+//    }
+}
